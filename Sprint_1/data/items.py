a12ca8b53c66a4ffbe2c31ce7891efc11f2993be
Medina Zabergja <132892106+MedinaZabergja@users.noreply.github.com>
2025-05-08 12:11:52 +0200

    Create items.py

---
+++ Sprint_1/data/items.py
@@ -0,0 +1,28 @@
+# data/items.py
+
+items = [
+    {
+        "id": 1,
+        "type": "Blazer",
+        "brand": "Zara",
+        "color": "Black",
+        "category": "Outerwear",
+        "image_url": "https://example.com/images/black-blazer.png"
+    },
+    {
+        "id": 2,
+        "type": "White T-shirt",
+        "brand": "H&M",
+        "color": "White",
+        "category": "Top",
+        "image_url": "https://example.com/images/white-tshirt.png"
+    },
+    {
+        "id": 3,
+        "type": "Jeans",
+        "brand": "Levi’s",
+        "color": "Blue",
+        "category": "Bottom",
+        "image_url": "https://example.com/images/blue-jeans.png"
+    }
+]

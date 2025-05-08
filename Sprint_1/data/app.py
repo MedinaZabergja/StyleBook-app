82a4c27b8b728651c13356e29a28dea71c043e45
Medina Zabergja <132892106+MedinaZabergja@users.noreply.github.com>
2025-05-08 12:13:35 +0200

    Update app.py

--- Sprint_1/data/app.py
+++ Sprint_1/data/app.py
@@ -8,3 +8,14 @@ app = Flask(__name__)
 @app.route("/api/items")
 def get_items():
     return jsonify(items)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/wardrobe")
+def wardrobe():
+    return render_template("wardrobe.html", items=items)
+
+if __name__ == "__main__":
+    app.run(debug=True)
